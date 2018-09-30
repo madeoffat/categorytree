cdd80ec22d5e8a8b8844bec31486e90e2a92f49d
‘yoneyama’ <gooner.c.f.25@gmail.com>
2018-09-30 19:11:29 +0900

    Initailize repository

---
+++ src/main/java/com/example/categorytree/CategorytreeApplication.java
@@ -0,0 +1,12 @@
+package com.example.categorytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorytreeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/categorytree/controller/CategoryController.java
@@ -0,0 +1,5 @@
+package com.example.categorytree.controller;
+
+public class CategoryController {
+
+}
