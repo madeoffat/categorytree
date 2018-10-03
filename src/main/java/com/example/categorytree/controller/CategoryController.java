feb40122a11efa4c50160151aac49ddff501f48d
米山和志 <madeoffat@MyMBP13.local>
2018-10-03 22:37:52 +0900

    category-tree-1

--- src/main/java/com/example/categorytree/controller/CategoryController.java
+++ src/main/java/com/example/categorytree/controller/CategoryController.java
@@ -1,5 +1,28 @@
 package com.example.categorytree.controller;
 
-public class CategoryController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.categorytree.entity.Category;
+import com.example.categorytree.service.CategoryService;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+	
+	@Autowired
+	CategoryService categoryService;
+	
+    @GetMapping
+    public String list(Model model) throws InterruptedException {
+    		List<Category> genres = categoryService.list();
+    		System.out.println(genres);
+    		model.addAttribute("categories", genres);
+    		return "categories/list";
+    }
 }
