df207a711b010c72f2da4136cd0be46f1fee08a9
米山和志 <madeoffat@MyMBP13.local>
2018-10-03 22:59:35 +0900

    fix to exception

---
+++ src/main/java/com/example/categorytree/Config.java
@@ -0,0 +1,21 @@
+package com.example.categorytree;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.categorytree.repository.CategoryRepository;
+import com.example.categorytree.service.CategoryService;
+
+@Configuration
+public class Config {
+	
+//	@Bean
+//	CategoryService categoryService() {
+//		return new CategoryService();
+//	}
+//	
+//	@Bean
+//	CategoryRepository categoryRepository() {
+//		return new CategoryRepository();
+//	}
+}
